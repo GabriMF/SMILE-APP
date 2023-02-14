0e6b89be5871700514d767ab54b5611960fef049
Ramiro-isla <ramiroisla11@gmail.com>
2023-02-14 10:28:48 +0100

    profile and patients models done

--- src/main/java/com/example/SMILE/models/profile.java
+++ src/main/java/com/example/SMILE/models/profile.java
@@ -8,12 +8,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "patients")
-public class patients {
+@Table(name = "profiles")
+public class profile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_patient")
+    @Column(name = "id_profile")
     private Long id;
     @Column(name = "dni")
     private String dni;
@@ -21,35 +21,78 @@ public class patients {
     private String name;
     @Column(name = "last_name")
     private String lastName;
-    public patients(String dni, String name, String lastName) {
+    @Column(name = "age")
+    private int age;
+    @Column(name = "phone_number")
+    private int phoneNumber;
+    @Column(name = "address")
+    private String address;
+
+    public profile(String dni, String name, String lastName, int age, int phoneNumber, String address) {
         this.dni = dni;
         this.name = name;
         this.lastName = lastName;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getDni() {
         return dni;
     }
+
     public void setDni(String dni) {
         this.dni = dni;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // dni nombre apellido edad telefono y calle
 
 }
