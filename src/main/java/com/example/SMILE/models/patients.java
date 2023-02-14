1b71bceacd55ef6ed14d7a9fe1f0c42ee79767e2
Ramiro-isla <ramiroisla11@gmail.com>
2023-02-14 09:56:44 +0100

    models patients in progress

---
+++ src/main/java/com/example/SMILE/models/patients.java
@@ -0,0 +1,18 @@
+package com.example.SMILE.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy.Strategy;
+
+@Entity
+@Table(name = "patients")
+
+
+public class patients {
+
+    @GeneratedValue(strategy =)
+
+}
