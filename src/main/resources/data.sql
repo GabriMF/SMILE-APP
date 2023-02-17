8c7dfd36d3e4c728f3801f734e62cb456fcd7012
Ramiro-isla <ramiroisla11@gmail.com>
2023-02-17 09:25:44 +0100

    data sql done

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO cities( id_cities, dni, cities) VALUES (default,'8749653C', 'Madrid')
+INSERT INTO cities( id_cities, dni, cities) VALUES (default,'483657N', 'Barcelona')
+
+INSERT INTO patients( id_patient, dni, name, last_name) VALUES (default,'8749653C', 'Pedro', 'Perez')
+INSERT INTO patients( id_patient, dni, name, last_name) VALUES (default,'483657N', 'María', 'Villaverde')
+
+INSERT INTO profiles( id_profile, dni, name, last_name, age, phone_number, address) VALUES (default,'8749653C', 'Pedro', 'Perez', 20, 695453795, 'Calle de la Plata 25')
+INSERT INTO profiles( id_profile, dni, name, last_name, age, phone_number, address) VALUES (default,'483657N', 'María', 'Villaverde', 24, 694576593, 'Calle Villa azul 7')
+
+INSERT INTO treatments( id_treatments, dni, treatments) VALUES (default,'483657N', 'Limpieza dental')
+INSERT INTO treatments( id_treatments, dni, treatments) VALUES (default,'483657N', 'Braquets')
